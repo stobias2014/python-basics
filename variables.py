d0b63b79d56db3a88619eab5bb6dcddcca9cf3cb
Saul Tobias <saul.tobias@fdmgroup.com>
2020-06-14 18:30:06 -0500

    init commit

---
+++ variables.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+number = 100
+name = "Python"
+dec_number = 3.14
+
+print(number)
+print(name)
+print(dec_number)
+
+
+print(name[0])
+print(name[0:2])
+print(name[2:])
+print(name * 2)
+print(name + "TEST")
+
